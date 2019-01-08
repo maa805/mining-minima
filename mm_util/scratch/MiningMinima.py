1e9f98fbb32dc721dd3969774c75382bd49c6053
maa805 <maadrianowycz@gmail.com>
2019-01-08 14:06:57 -0800

    add mode scanning function

--- mm_util/scratch/MiningMinima.py
+++ mm_util/scratch/MiningMinima.py
@@ -24,7 +24,6 @@ class MiningMinima:
     '''
         
 		KT_IN_KCAL = 0.6163
-		self.kT = 1.0
 		
 		if !infile: 
 
@@ -37,7 +36,7 @@ class MiningMinima:
 		if seq1: self.movemap, self.dof_dict = pose_setup_turner(self, self.input_pose, seq1, seq2)
 		else: self.movemap, self.dof_dict = pose_setup_from_file(self, self.input_pose, infile)
 		
-		N_DOFS = len(dof_dict)
+		self.N_DOFS = len(dof_dict)
 		
 		self.scorefxn = core.scoring.ScoreFunctionFactory.create_score_function(scorefxn)
 		
@@ -50,20 +49,36 @@ class MiningMinima:
 		self.eigenvalues, self.modes = np.linalg.eigh(self.hessian)
 		
 		# Calculate free energy
-		self.harmonic_free_energy = self.min_energy - 0.5*N_DOFS*np.log(2.*np.pi*self.kT) + 0.5*np.log(np.product(self.eigenvalues))
-		self.free_energy = mode_scan(self, self.min_pose, self.scorefxn, self.dof_dict)
+		self.harmonic_free_energy = self.calc_harmonic_free_energy(self)
+		self.free_energy, _ = mode_scan(self, self.min_pose, self.scorefxn, self.dof_dict)
 		
 	
 	
 	
 	def pose_setup_turner(self, self.input_pose, self.seq1, self.seq2)
 	
+		return movemap, dof_dict
+	
     def pose_setup_from_file(self, self.input_pose, self.infile) 
 	
+		return movemap, dof_dict
+		
 	def find_minimum(self, self.min_pose, self.scorefxn)
 	
 	def calc_hessian_at_min(self, self.min_pose, self.scorefxn, self.dof_dict)
 	
 	def mode_scan(self, self.min_pose, self.scorefxn, self.dof_dict)
 	
-	def calc_harmonic_free_energy
+	def calc_harmonic_free_energy(self, self.min_energy, self.N_DOFS, self.eigenvalues, kt = 1.0)
+	
+		return -kt*(self.min_energy - 0.5*self.N_DOFS*np.log(2*np.pi*kt) + 0.5*self.N_DOFS*np.log(np.prod(self.eigenvalues)))
+
+def pose_setup_from_file(pose, infile)
+
+	#TODO: Figure out how to determine whether is ss, ds, or something else and process accordingly 
+
+def find_minimum(min_pose, scorefxn, movemap)
+
+	#HMMMM maybe let user specify a particular minmover, otherwise just setup a default minmover? we'll worry about this later
+	minmover = MinMover(
+	
---
+++ mm_util/scratch/mode_scan.py
@@ -0,0 +1,43 @@
+import numpy as np
+import scipy
+
+def mode_scan(min_pose, mode, scorefxn, dof_dict, h = 1.0, range = 60.0, kt = 1.0)
+'''
+Given an input pose (assumed to be a local minimum) and a correspondig mode, returns the partition
+function evaluated along the mode.
+'''
+
+## Initialize dofs from input pose and dof_dict -- list comprehension?
+## We should probably return the scanned modes for manipulation, as well as a calculated free energy
+
+	# Initialize stuff 
+	min_energy = scorefxn(min_pose)
+	energy = min_energy
+	
+	temp = Pose()
+	temp.assign(pose_min)
+	
+	# Read off dofs from input pose
+	dofs = [pose_min.torsion(dof_dict{key}) for key in dof_dict]
+	
+	ii = 0
+	result = np.array([])
+	
+	# h is in degrees to match rosetta movers
+	h = np.arange(-range, range+h, h)
+	
+	# Scan along mode at indicated granularity 
+	for hh in h:
+	
+		dofs_new = dofs + hh*mode
+		for key, val in enumerate(dofs_new): temp.set_torsion(dof_dict[key], val)
+		
+		result = np.append(result, np.exp(scorefxn(temp) - E_0)/kt)
+	
+	# Apply trapezoidal rule to get anharmonic free energy
+	partition_function = np.trapz(resutl, x=h*np.pi/180.0)
+	
+	return partition_function, result 
+	
+	
+	
