68103aaa2aead47b70cd437a0ed65b74184467ee
maa805 <maadrianowycz@gmail.com>
2019-05-16 15:53:09 -0700

    central difference scheme for second order partials

--- MiningMinima/scratch/calc_hessian_at_min.py
+++ MiningMinima/scratch/calc_hessian_at_min.py
@@ -21,7 +21,9 @@ def calc_hessian_at_min(pose_min, scorefxn, dof_dict):
 
     hessian_at_min = np.zeros((len(dofs), len(dofs)))
 
-    n_pts = int(sig/h + 1)
+    n_pts = int(sig/h)
+    if n_pts % 2 == 0: n_pts += 1
+	
     ind = int(n_pts/2)
 
     tor_ranges = np.zeros((n_pts, len(dofs)))
@@ -53,19 +55,35 @@ def calc_hessian_at_min(pose_min, scorefxn, dof_dict):
 
         hess = numpy_hessian(energy, h_rad)
 
-        d2E_dx2 =  hess[0,0,:,:]
-        d2E_dy2 =  hess[1,1,:,:]
+        #d2E_dx2 =  hess[0,0,:,:]
+        #d2E_dy2 =  hess[1,1,:,:]
         d2E_dxdy = hess[0,1,:,:]
 
-        if hessian_at_min[foo, foo] == 0:
+        #if hessian_at_min[foo, foo] == 0:
 
-            hessian_at_min[foo, foo] = d2E_dx2[ind, ind]
+        #    hessian_at_min[foo, foo] = d2E_dx2[ind, ind]
 
-        if hessian_at_min[bar, bar] == 0:
+        #if hessian_at_min[bar, bar] == 0:
 
-            hessian_at_min[bar, bar] = d2E_dy2[ind, ind]
+        #    hessian_at_min[bar, bar] = d2E_dy2[ind, ind]
 
         hessian_at_min[foo, bar] = d2E_dxdy[ind, ind]
         hessian_at_min[bar, foo] = d2E_dxdy[ind, ind]
-        
+    
+	pose.assign(pose_min)
+	
+    for key in dof_dict:
+		
+        energy = np.zeros((n_pts, 1))
+        x = tor_ranges[:, key]
+		
+        for ii, xx in enumerate(x):
+		
+            pose.set_torsion(dof_dict[key], xx)
+            energy[ii] = scorefxn(pose)
+            pose.set_torsion(dof_dict[key], dofs[key])
+			
+	
+        hessian_at_min[key, key] = (energy[ind+1] - 2*energy[ind] + energy[ind-1])/h_rad**2
+		
     return hessian_at_min
